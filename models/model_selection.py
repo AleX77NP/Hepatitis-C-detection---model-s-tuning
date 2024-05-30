69b2345d3d0cf8abc31e0c49f1722ff9211479a4
Aleksandar Milanovic <milanovicaleX77@gmail.com>
2024-05-30 15:18:53 +0200

    add bias variance tradeoff graph

--- models/model_selection.py
+++ models/model_selection.py
@@ -6,14 +6,14 @@ from sklearn.ensemble import RandomForestClassifier
 class ModelSelector:
     # Init all models with specified hyperparams
     def __init__(self, hyperparams: dict) -> None:
-        self.models = {
-            'logistic': self.__get_logistic_model(hyperparams=hyperparams['logistic']),
-            'n_bayes': self.__get_naive_bayes(hyperparams=hyperparams['n_bayes']),
-            'rand_forest': self.__get_random_forest(hyperparams=hyperparams['rand_forest'])
+        self.__models = {
+            'logistic': self.__get_logistic_model(hyperparams=hyperparams['logistic']) if hyperparams.get('logistic') else None,
+            'n_bayes': self.__get_naive_bayes(hyperparams=hyperparams['n_bayes']) if hyperparams.get('n_bayes') else None,
+            'rand_forest': self.__get_random_forest(hyperparams=hyperparams['rand_forest']) if hyperparams.get('rand_forest') else None
         }
     
     def select_model(self, model_name: str):
-        return self.models[model_name]
+        return self.__models[model_name]
     
     def __get_logistic_model(self, hyperparams: dict):
         return LogisticRegression(multi_class='multinomial', solver=hyperparams['solver'], 
--- utils/dataset.py
+++ utils/dataset.py
@@ -1,5 +1,28 @@
 import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
 
 def load_dataset(file: str) -> pd.DataFrame:
     df: pd.DataFrame = pd.read_csv(file)
     return df
+
+def split_data(X, label, test_size=0.25, random_state=2):
+    y=X[label]
+    unique_classes = np.unique(y)
+    X_train = []
+    X_test = []
+    y_train = []
+    y_test = []
+    for class_value in unique_classes:
+        X_class = X[y == class_value]
+        y_class = y[y == class_value]
+        X_class_train, X_class_test, y_class_train, y_class_test = train_test_split(X_class, y_class, test_size=test_size, random_state=random_state)
+        X_train.append(X_class_train)
+        X_test.append(X_class_test)
+        y_train.append(y_class_train)
+        y_test.append(y_class_test)
+    X_train = np.concatenate(X_train)
+    X_test = np.concatenate(X_test)
+    y_train = np.concatenate(y_train)
+    y_test = np.concatenate(y_test)
+    return X_train, X_test, y_train, y_test
