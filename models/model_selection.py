4e6fd332b85baed975dd07114001d1ea9a7d9225
Aleksandar Milanovic <milanovicaleX77@gmail.com>
2024-05-31 15:39:17 +0200

    Add gradient boost algorithm

--- models/model_selection.py
+++ models/model_selection.py
@@ -1,5 +1,5 @@
 from sklearn.linear_model import LogisticRegression
-from sklearn.naive_bayes import MultinomialNB
+from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.ensemble import RandomForestClassifier
 
 # Class used to return specific ML model
@@ -8,7 +8,7 @@ class ModelSelector:
     def __init__(self, hyperparams: dict) -> None:
         self.__models = {
             'logistic': self.__get_logistic_model(hyperparams=hyperparams['logistic']) if hyperparams.get('logistic') else None,
-            'n_bayes': self.__get_naive_bayes(hyperparams=hyperparams['n_bayes']) if hyperparams.get('n_bayes') else None,
+            'g_boost': self.__get_g_boost(hyperparams=hyperparams['g_boost']) if hyperparams.get('g_boost') else None,
             'rand_forest': self.__get_random_forest(hyperparams=hyperparams['rand_forest']) if hyperparams.get('rand_forest') else None
         }
     
@@ -19,8 +19,8 @@ class ModelSelector:
         return LogisticRegression(multi_class='multinomial', solver=hyperparams['solver'], 
                 max_iter=hyperparams['max_iter'], C=hyperparams['C'])
     
-    def __get_naive_bayes(self, hyperparams: dict):
-        return MultinomialNB(alpha=hyperparams['alpha'])
+    def __get_g_boost(self, hyperparams: dict):
+        return GradientBoostingClassifier(n_estimators=hyperparams['n_estimators'], learning_rate=hyperparams['l_rate'])
     
     def __get_random_forest(self, hyperparams: dict):
         return RandomForestClassifier(n_estimators=hyperparams['n_estimators'], 
